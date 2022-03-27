8863d6b33fea4f9e7a9b6f0fea82bdb7f612ca89
AlejandroDeWolf <dewolf.alejandro@gmail.com>
2022-03-27 14:57:55 +0200

    feat: create route to update message function

--- routers/messages.js
+++ routers/messages.js
@@ -2,8 +2,14 @@ const express = require('express');
 const router = express.Router();
 const messagesController = require("./../controllers/messages");
 
+
 router.get('/api/v1/messages', messagesController.getAll);
+
 router.get('/api/v1/messages/:id', messagesController.getId);
+
 router.post("/api/v1/messages", messagesController.create);
+
+router.put("/api/v1/messages/:id", messagesController.update);
+
 
 module.exports = router;
